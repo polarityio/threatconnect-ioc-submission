24f927dc57ca1612bc71492e23a44e4133006cf3
Jon Penwood <jonp@polarity.io>
2020-12-04 13:52:38 -0600

    Made onMessage not async

--- integration.js
+++ integration.js
@@ -41,8 +41,7 @@ const doLookup = async (entities, { url, ..._options }, cb) => {
   cb(null, lookupResults);
 };
 
-
-const onMessage = async ({ data: { action, ...actionParams} }, options, callback) => {
+const onMessage = ({ data: { action, ...actionParams} }, options, callback) => {
   if (action === 'deleteItem') {
     deleteItem(actionParams, requestWithDefaults, options, Logger, callback);
   } else if (action === 'submitItems') {
