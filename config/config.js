509f05dac68757dd68b68ec3dd145ae2d9af3f1e
Jon Penwood <jonp@polarity.io>
2024-10-17 12:14:50 -0500

    fix: Synced config entity types & Updated Deps

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   acronym: 'TC+',
   description:
     'Polarity integration that connects to the ThreatConnect threat intelligence platform using the IOC Submission interface format.',
-  entityTypes: ['domain', 'IPv4', 'IPv6', 'hash', 'email', 'url'],
+  entityTypes: ['domain', 'IPv4', 'IPv6', 'MD5', 'SHA1', 'SHA256', 'email', 'url'],
   defaultColor: 'light-pink',
   styles: ['./styles/styles.less'],
   block: {
